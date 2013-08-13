a503796fd6284b9db91e52f7e26841f9e6994251
lihudi <lmgamo@iot.es>
2013-08-13 16:51:45 +0200

    Commit to look for problems

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Luis Miguel Gamo Lopez on 13/08/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
